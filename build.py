80c345509fdb41a6ddad6ac1c4cbddff3fd7ec52
Eric He <yihuahe.50@gmail.com>
2024-03-23 22:14:45 +1300

    Updated build.py to build core shaders

--- build.py
+++ build.py
@@ -1,10 +1,16 @@
 import os
 import subprocess
 
-# Compile shader
-# compile_shader_script_path = "../src/core/graphics/script/compile_shader.py"
-# subprocess.check_call(["py", "-3", compile_shader_script_path, "../src/shaders/vertex.glsl"], stderr=subprocess.STDOUT)
+# Compile core shaders
+current_dir = os.path.dirname(os.path.realpath(__file__))
+compile_shader_script_path = current_dir + "/src/core/graphics/script/compile_shader.py"
+core_shader_dir = current_dir + "/src/core//graphics/shaders"
 
+for file in os.listdir(core_shader_dir):
+	if file.endswith(".vert") or file.endswith(".frag") or file.endswith(".comp"):
+		subprocess.check_call(["py", "-3", compile_shader_script_path, os.path.join(core_shader_dir, file)], stderr=subprocess.STDOUT)
+
+# Build project
 build_dir = "./build"
 
 if not os.path.isdir(build_dir):
