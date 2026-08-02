6270650173ba511c1b408595827e7475adb9b5b9
50CaL <ericho890203@gmail.com>
2026-08-02 19:22:23 +1200

    Show level and XP on the status panel HUD. (#15)
    Bind ExperienceComponent fields through UIDataModel and place
    kill count at the bottom of the panel.
    
    Co-authored-by: Eric He <yihuahe.50@gmail.com>

--- src/game/ui/StatusPanel.cpp
+++ src/game/ui/StatusPanel.cpp
@@ -3,7 +3,9 @@
 #include <ecs/ECS.h>
 #include <ui/UIDataModel.h>
 
+#include "../components/ExperienceComponent.h"
 #include "../components/PlayerComponents.h"
+
 
 using namespace vortex;
 
@@ -11,6 +13,9 @@ StatusPanel::StatusPanel( eage::ecs::ECSRegistry& registry, eage::ui::UIDataMode
 	: mRegistry( registry )
 	, mModel( model )
 {
+	mModel.Declare( "current_level", 1 );
+	mModel.Declare( "current_xp", 0 );
+	mModel.Declare( "xp_to_lvl_up", 5 );
 	mModel.Declare( "kill_count", 0 );
 }
 
@@ -20,5 +25,13 @@ StatusPanel::Update()
 	for( auto [ entity, player ] : mRegistry.GetComponentMap<PlayerComponent>() )
 	{
 		mModel.Set( "kill_count", player.kill_count );
+
+		if( mRegistry.HasComponent<ExperienceComponent>( entity ) )
+		{
+			const auto& experience = mRegistry.GetComponent<ExperienceComponent>( entity );
+			mModel.Set( "current_level", experience.current_level );
+			mModel.Set( "current_xp", experience.current_xp );
+			mModel.Set( "xp_to_lvl_up", experience.xp_to_lvl_up );
+		}
 	}
 }
