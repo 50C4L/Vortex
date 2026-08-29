6d8c3f80a878464b5c1f30f2153bc3f8048a92b7
Eric He <yihuahe.50@gmail.com>
2026-08-29 14:00:00 +1200

    core unit test target with doctest

---
+++ src/core/tests/PlaceholderTest.cpp
@@ -0,0 +1,6 @@
+#include <doctest/doctest.h>
+
+TEST_CASE( "placeholder" )
+{
+	CHECK( 1 + 1 == 2 );
+}
---
+++ src/core/tests/TestMain.cpp
@@ -0,0 +1,3 @@
+#define SDL_MAIN_HANDLED
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include <doctest/doctest.h>
